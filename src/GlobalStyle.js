129664017e45a588e2a80e691537c4863d23f071
jtduchesne <jtduchesne@gmail.com>
2021-06-04 18:01:50 -0400

    Add color palette to GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,6 +2,20 @@ import { createGlobalStyle } from "styled-components";
 import { normalize } from "styled-normalize";
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --orange: #E87A00;
+    --taupe: #8C756A;
+
+    --mist: #90AFC5;
+    --teal: #0292b7;
+
+    --white: #F8F8F8;
+    --foam: #BEC6DA;
+    --darkgray: #2F3136;
+    --shadow: #24272B;
+    --black: #111111;
+  }
+
   ${normalize}
 
   html {
