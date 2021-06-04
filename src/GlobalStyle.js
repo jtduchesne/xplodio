e9d14205254fdfa05d53e85913aeb3f739dc7191
jtduchesne <jtduchesne@gmail.com>
2021-06-04 14:59:12 -0400

    Install ‘styled-normalize’
    CSS-normalize library for styled-components.

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
   html {
     box-sizing: border-box;
   }
@@ -12,8 +15,6 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    margin: 0;
-    padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
